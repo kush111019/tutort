65c1f023d7067cc755aa858f3d6d99c3be918703
kiran-mobilefirst <kiran.darode@mobilefirstapplications.com>
2025-11-01 09:33:13 +0530

    first commit

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass{
+    public static void main(String gg[]){
+        for(int i = 1 ; i <= 5 ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ eg1.java
@@ -0,0 +1,13 @@
+public class eg1 {
+    public static void main(String gg[]){
+        int a = 1;
+        for(int i = 1 ; i <= 5 ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(a);
+                System.out.print("\t");
+                a++;
+            }
+            System.out.print("\n");
+        }
+    }
+}
