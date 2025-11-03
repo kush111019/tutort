5f95d65ebeb856999010808549e4d90df8b02269
kush sharma <kush.barotha@gmail.com>
2025-11-03 22:36:23 +0530

    third commit

---
+++ eg4.java
@@ -0,0 +1,13 @@
+class eg4 {
+public static void main(String gg[])
+{
+ for(int i = 1 ; i <= 5 ; i++)
+ {
+  for(int j = 5 ; j >= i ; j--)
+  {
+   System.out.print(j + " ");
+  }
+  System.out.println();
+ }
+}
+}
---
+++ eg5.java
@@ -0,0 +1,13 @@
+class eg5 {
+public static void main(String gg[])
+{
+ for(int i = 1 ; i <= 5 ; i++)
+ {
+  for(int j = i ; j <= 5 ; j++)
+  {
+   System.out.print(j % 2 + " ");
+  }
+  System.out.print("\n");
+ }
+}
+}
